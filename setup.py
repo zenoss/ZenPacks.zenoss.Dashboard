2c63187611f02b4aa4c1bb4674b3b608dc676061
Paco Ibanez-Castillo <fraibacas@gmail.com>
2016-07-27 11:22:24 -0500

    Bump to dev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Dashboard"
-VERSION = "1.2.1"
+VERSION = "1.2.2dev"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
