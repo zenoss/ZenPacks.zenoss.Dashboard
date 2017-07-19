e3fb7a8f0009afb57734b1dd707b09fc77ac0521
ostyhar <ostyhar@zenoss.com>
2017-07-19 14:36:41 -0400

    ZEN-25304: RM Site Portal url points to wrong page

--- ZenPacks/zenoss/Dashboard/migrate/updateDefaultSiteUrl.py
+++ ZenPacks/zenoss/Dashboard/migrate/updateDefaultSiteUrl.py
@@ -20,13 +20,11 @@ class updateDefaultSiteUrl(ZenPackMigration):
     version = Version(1, 2, 6)
 
     def migrate(self, pack):
-        try:
+        if hasattr(pack.dmd.ZenUsers, 'dashboards'): 
             DEFAULT_DASHBOARD_STATE = '[{"id":"col-0","items":[{"title":"Welcome to Zenoss!","refreshInterval":3000,"config":{"siteUrl":"https://www2.zenoss.com/in-app-welcome?v=4.9.70&p=%s"},"xtype":"sitewindowportlet","height":399,"collapsed":false},{"title":"Google Maps","refreshInterval":300,"config":{"baselocation":"/zport/dmd/Locations","pollingrate":400},"xtype":"googlemapportlet","height":400,"collapsed":false}]},{"id":"col-1","items":[{"title":"Open Events","refreshInterval":300,"config":{"stateId":"ext-gen1351"},"xtype":"eventviewportlet","height":400,"collapsed":false},{"title":"Open Events Chart","refreshInterval":300,"config":{"eventClass":"/","summaryFilter":"","daysPast":3},"xtype":"openeventsportlet","height":400,"collapsed":false}]}]'
             default = pack.dmd.ZenUsers.dashboards._getOb('default', None)
             if default:
                 site={'core': 'core', 'enterprise': 'commercial'}[pack.dmd.getProductName()]
                 default.state = DEFAULT_DASHBOARD_STATE % site
-        except Exception as e:
-            log.warning('Migrate script updateDefaultSiteUrl failed due to %s' % e.message)
 
 updateDefaultSiteUrl()
